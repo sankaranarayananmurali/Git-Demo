3bf58ce8b1ca36445a1f01b3b94e1198b6eb6a56
sankaranarayanan <msankaranarayananece@outlook.com>
2024-12-09 20:42:59 +0530

    index.html and Demo.java is commited by sankaranarayanan

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public Class demo{
+
+    public static void main(String[] args){
+        System.out.println("Hello Everyone ")
+
+    int a =10;
+    int b=23;
+    System.out.println("Sum of a and b is: "a+b);
+    }
+
+}
